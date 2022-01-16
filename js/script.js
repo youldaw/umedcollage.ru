7182d10c394a853cad6e82c7c5f1a51fa554d695
youldaw <68648932+youldaw@users.noreply.github.com>
2022-01-16 20:34:47 +0500

    index ready
    index ready

--- js/script.js
+++ js/script.js
@@ -1,31 +1,54 @@
 $(function (){
     // $('input[name=phone]').mask('+7 (999) 999-99-99');
 
-    // $('.main-slide').owlCarousel({
-    //     loop:true,
-    //     // autoplay : true,
-    //     // autoplayTimeout : 8000,
-    //     margin:0,
-    //     nav:true,
-    //     dots:true,
-    //     center: false,
-    //     smartSpeed:900,
-    //     items:1,
-    //     navText: ['<img src="images/arrow-left.png">','<img src="images/arrow-left.png">'],
-    //     responsive:{
-    //         0:{
-    //             items:1,
-    //         },
-    //         600:{
-    //             items:1,
-    //             margin:0,
-    //         },
-    //         1000:{
-    //             items:1,
-    //             margin:0,
-    //         }
-    //     }
-    // });
+    $('.news-slide').owlCarousel({
+        loop:true,
+        // autoplay : true,
+        // autoplayTimeout : 8000,
+        margin:20,
+        nav:true,
+        dots:true,
+        center: false,
+        smartSpeed:900,
+        items:2,
+        navText: ['<img src="images/slide-left-arrow.png">','<img src="images/slide-right-arrow.png">'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:2,
+                margin:20,
+            }
+        }
+    });
+
+    $('.students-slide').owlCarousel({
+        loop:true,
+        // autoplay : true,
+        // autoplayTimeout : 8000,
+        margin:20,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:4,
+        navText: ['<img src="images/slide-left-arrow.png">','<img src="images/slide-right-arrow.png">'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });
 
 
     $('.menu-mob, .menu-closer').on('click',function(e){
