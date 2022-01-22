5aeac04949f0d41cdb205a5e2dcebc82d7491735
youldaw <68648932+youldaw@users.noreply.github.com>
2022-01-23 01:07:38 +0500

    desktop tugadi
    desktop tugadi

--- js/script.js
+++ js/script.js
@@ -28,8 +28,6 @@ $(function (){
 
     $('.students-slide').owlCarousel({
         loop:true,
-        // autoplay : true,
-        // autoplayTimeout : 8000,
         margin:20,
         nav:true,
         dots:false,
@@ -52,8 +50,6 @@ $(function (){
 
     $('.spec-slide').owlCarousel({
         loop:false,
-        // autoplay : true,
-        // autoplayTimeout : 8000,
         margin:20,
         nav:true,
         dots:false,
@@ -76,8 +72,6 @@ $(function (){
 
     $('.event-slide').owlCarousel({
         loop:false,
-        // autoplay : true,
-        // autoplayTimeout : 8000,
         margin:20,
         nav:true,
         dots:false,
@@ -98,15 +92,162 @@ $(function (){
         }
     });
 
+    $('.inner-slide-one').owlCarousel({
+        loop:false,
+        margin:20,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:2,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1.5,
+            },
+            1000:{
+                items:2,
+            }
+        }
+    });
+
+    $('.inner-slide-two').owlCarousel({
+        loop:false,
+        margin:0,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:1,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    $('.additional-slide').owlCarousel({
+        loop:false,
+        margin:20,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:3,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    $('.sa-slide').owlCarousel({
+        loop:false,
+        margin:20,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:2,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1.5,
+            },
+            1000:{
+                items:2,
+            }
+        }
+    });
+
+    $('.why-slide').owlCarousel({
+        loop:false,
+        margin:20,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:1,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    $('.nh-slide').owlCarousel({
+        loop:true,
+        margin:40,
+        nav:false,
+        dots:false,
+        // center: true,
+        smartSpeed:900,
+        items:1,
+        navText: ['<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 1L1.34315 6.65685L7 12.3137" stroke="#5E419D" stroke-width="1.5"/></svg></span>','<span><svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.65685 6.65685L1 12.3137" stroke="white" stroke-width="1.5"/></svg></span>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1.4,
+            }
+        }
+    });
+    
+
 
     $('.accordion-button').on('click',function(e){
         e.preventDefault();
         $(this).toggleClass('active');
     });
 
-    $('.menu-mob, .menu-closer').on('click',function(e){
+    $('.menu-opener a').on('click',function(e){
         e.preventDefault();
-        $('.top-left').toggleClass('active');
+        $(this).toggleClass('active');
+        $('.left-menu').toggleClass('minimized');
+    });
+
+    $('.search-opener').on('click',function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.searchbox').toggleClass('active');
+    });
+    $(document).mouseup(function (e){
+        var div = $(".searchbox, .search-opener");
+        if (!div.is(e.target) 
+            && div.has(e.target).length === 0) {
+            div.removeClass('active');
+        }
     });
 
     
